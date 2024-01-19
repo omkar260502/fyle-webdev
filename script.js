8027986c56816165a0adc5d91c32839c79ff37a2
Omkar Shashikant Taple <omkartaple.3@gmail.com>
2024-01-19 12:11:32 +0530

    features added

--- script.js
+++ script.js
@@ -10,8 +10,19 @@ let reposPerPage = 10;
 let currentPage = 1;
 let totalPages = 1;
 
+const loader = document.getElementById('loader');
+
+const showLoader = () => {
+    loader.style.display = 'block';
+};
+
+const hideLoader = () => {
+    loader.style.display = 'none';
+};
+
 const getUser = async (username, page = 1) => {
     try {
+        showLoader(); 
         const userResponse = await fetch(`${APIURL}${username}`);
         const userData = await userResponse.json();
 
@@ -21,6 +32,8 @@ const getUser = async (username, page = 1) => {
         getRepos(username, page);
     } catch (error) {
         console.error('Error fetching user data:', error);
+    } finally {
+        hideLoader(); 
     }
     renderPagination(currentUser, totalRepos);
 };
@@ -71,20 +84,35 @@ const renderUserCard = (data) => {
 
             ${data.blog ? `
                 <p>
-                    <svg class="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
-                        <path fill-rule="evenodd" d="M11.75 1a1.75 1.75 0 1 1 0 3.5 1.75 1.75 0 0 1 0-3.5zM1.75 4.25a1.75 1.75 0 1 1 3.5 0 1.75 1.75 0 0 1-3.5 0zM4.5 14a1.75 1.75 0 0 1 0-3.5 1.75 1.75 0 0 1 0 3.5zM14 11.75a1.75 1.75 0 1 1-3.5 0 1.75 1.75 0 0 1 3.5 0zM11.47 3.22a3 3 0 0 1 0 4.24l-7.78 7.78a3 3 0 0 1-4.24 0 3 3 0 0 1 0-4.24l7.78-7.78a3 3 0 0 1 4.24 0z"></path>
-                    </svg>
-                    <a href="${data.blog}" target="_blank">${data.blog}</a>
+                <svg class="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M11.75 1a1.75 1.75 0 1 1 0 3.5 1.75 1.75 0 0 1 0-3.5zM1.75 4.25a1.75 1.75 0 1 1 3.5 0 1.75 1.75 0 0 1-3.5 0zM4.5 14a1.75 1.75 0 0 1 0-3.5 1.75 1.75 0 0 1 0 3.5zM14 11.75a1.75 1.75 0 1 1-3.5 0 1.75 1.75 0 0 1 3.5 0zM11.47 3.22a3 3 0 0 1 0 4.24l-7.78 7.78a3 3 0 0 1-4.24 0 3 3 0 0 1 0-4.24l7.78-7.78a3 3 0 0 1 4.24 0z" style="fill: white;"></path>
+                </svg>
+                    <a href="${data.blog}" target="_blank" style="color: white; text-decoration: none;">${data.blog}</a>
                 </p>
             ` : ''}
+
+            <!-- Add social links here -->
+            ${data.twitter ? `<a href="https://twitter.com/${data.twitter}" target="_blank" style="color: white; text-decoration: none;"><img class="social-icon" src="twitter-icon.svg" alt="Twitter" style="fill: white;"></a>` : ''}
+            ${data.facebook ? `<a href="https://facebook.com/${data.facebook}" target="_blank" style="color: white; text-decoration: none;"><img class="social-icon" src="facebook-icon.svg" alt="Facebook" style="fill: white;"></a>` : ''}
+            ${data.instagram ? `<a href="https://instagram.com/${data.instagram}" target="_blank" style="color: white; text-decoration: none;"><img class="social-icon" src="instagram-icon.svg" alt="Instagram" style="fill: white;"></a>` : ''}
+
+            <!-- Add GitHub homepage link -->
+            <p>
+                <svg class="octicon octicon-mark-github" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true" style="fill: white;">
+                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38C13.71 14.53 16 11.54 16 8c0-4.42-3.58-8-8-8Z"></path>
+                </svg>
+                <a href="${data.html_url}" target="_blank" style="color: white; text-decoration: none;">GitHub</a>
+            </p>
         </div>
     `;
 };
 
-const renderRepos = (repos) => {
+
+
+const renderRepos = async (repos) => {
     reposContainer.innerHTML = "";
 
-    repos.forEach((item) => {
+    for (const item of repos) {
         const repoCard = document.createElement("div");
         repoCard.classList.add("repo-card");
 
@@ -96,10 +124,27 @@ const renderRepos = (repos) => {
 
         const repoSkills = document.createElement("div");
         repoSkills.classList.add("repo-skills");
-        repoSkills.innerText = `Skills: ${item.language || "Not specified"}`;
+
+        const languagesUrl = `https://api.github.com/repos/${item.owner.login}/${item.name}/languages`;
+        const languagesResponse = await fetch(languagesUrl);
+
+        if (languagesResponse.ok) {
+            const languagesData = await languagesResponse.json();
+            const repoLanguages = Object.keys(languagesData);
+
+            repoLanguages.forEach((language) => {
+                const skillButton = document.createElement("button");
+                skillButton.classList.add("skill-button");
+                skillButton.innerText = language;
+
+                repoSkills.appendChild(skillButton);
+            });
+        } else {
+            repoSkills.innerText = "Unable to fetch languages";
+        }
 
         const repoLink = document.createElement("a");
-        repoLink.classList.add("repo-link");
+        repoLink.classList.add("repo-link", "view-repo"); 
         repoLink.href = item.html_url;
         repoLink.innerText = "View Repo";
         repoLink.target = "_blank";
@@ -110,8 +155,10 @@ const renderRepos = (repos) => {
         repoCard.appendChild(repoLink);
 
         reposContainer.appendChild(repoCard);
-    });
+    }
 };
+
+
 
 let reposPerPageSelect = document.querySelector("#reposPerPage");
 
